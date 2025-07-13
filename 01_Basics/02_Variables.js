f17abd191a69ba80a8ef87c6409c1a1a93ff5f02
Somya Keshri <keshrisomya9596@gmail.com>
2025-07-13 15:17:31 +0530

    variables

---
+++ 01_Basics/02_Variables.js
@@ -0,0 +1,12 @@
+var a = 67 // a contains 67
+console.log(a)
+a = "Buzz"         // (var can be updated & re-declared)
+console.log(a)
+
+var b = 5        // => declaring a variable
+// 5:literal , b:indentifier
+// (JS : Case sensitive)
+
+// let 8harry = 7 // Not allowed this will throw an error (begin with $, _ or a letter)
+// let var = 7   // syntax error (var is a reserved keyword)
+let harry = 53 // let can be updated but not re-declared
