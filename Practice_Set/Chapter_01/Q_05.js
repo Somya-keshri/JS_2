792af8025fd7c0f2471126da99cc7baaf9f9bd0e
Somya Keshri <keshrisomya9596@gmail.com>
2025-07-13 23:03:45 +0530

    ques_5

---
+++ Practice_Set/Chapter_01/Q_05.js
@@ -0,0 +1,11 @@
+// Write a JS proram to create a word meaning dictionary of 5 words
+
+const dict = {
+    "aberration": "a departure from what is normal, usual, or expected, typically an unwelcome one.",
+    "benevolent": "well meaning and kindly.",
+    "candid": "truthful and straightforward; frank.",
+    "diligent": "having or showing care and conscientiousness in one's work or duties.",
+    "ephemeral": "lasting for a very short time."
+}
+console.log(dict.benevolent);
+console.log(dict["candid"]);
