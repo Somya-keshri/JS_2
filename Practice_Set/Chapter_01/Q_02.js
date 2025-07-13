93f8c178e280e983ca1ab712783cb674cb00f347
Somya Keshri <keshrisomya9596@gmail.com>
2025-07-13 20:46:35 +0530

    ques_2

---
+++ Practice_Set/Chapter_01/Q_02.js
@@ -0,0 +1,6 @@
+// Use typeof operator to find the datatype of the string in last ques
+
+let a = "Hello ";
+let b = 5;
+console.log(a + b); // Output: Hello 5
+console.log(typeof (a+b)); // Output: string
