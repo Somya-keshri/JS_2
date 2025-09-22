fb1972faa832111dc3e6459b111e7213fdb5cb6b
Somya Keshri <keshrisomya9596@gmail.com>
2025-09-22 21:18:05 +0530

    variables

--- 01_Basics/02_Variables.js
+++ 01_Basics/02_Variables.js
@@ -1,7 +1,26 @@
 var a = 67 // a contains 67
+// var before ES6
+ 
 console.log(a)
 a = "Buzz"         // (var can be updated & re-declared)
 console.log(a)
+
+let q = 90
+console.log(q)
+q = "Hello"      // (let can be updated but not re-declared)
+// let q = "Hello"  // This will throw an error
+console.log(q)
+
+var p = 45;
+console.log(p)   // 45 
+{
+    var p = 'this';
+    console.log(p)    // this // var is function scoped
+    let q = 100;
+    console.log(q)    // 100 // let is block scoped
+}
+console.log(p)  // this // var is function scoped
+console.log(q)  // 100 // let is block scoped
 
 var b = 5        // => declaring a variable
 // 5:literal , b:indentifier
