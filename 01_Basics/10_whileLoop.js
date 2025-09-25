88de729335e45ec46796a3e390484374f878d529
Somya Keshri <keshrisomya9596@gmail.com>
2025-09-25 23:44:49 +0530

    loop

---
+++ 01_Basics/10_whileLoop.js
@@ -0,0 +1,15 @@
+let n = 7;
+let i = 0;
+while(i<n){
+    console.log(i);
+    i++;
+}
+// Note: If we do not increment i, then it will be an infinite loop
+
+let m = 5
+let j = 0
+do{
+    console.log(j);
+    j++;
+}while(j<m)
+// Note: In do while loop, the condition is checked after executing the loop body. So, the loop body is executed at least once even if the condition is false   
