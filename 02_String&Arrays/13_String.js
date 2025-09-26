f3e5b96fc2ef8e163fb9eff882baf2490f91341f
Somya Keshri <keshrisomya9596@gmail.com>
2025-09-26 19:36:17 +0530

    string

---
+++ 02_String&Arrays/13_String.js
@@ -0,0 +1,30 @@
+let name = "Jolly"
+console.log(name[0])
+console.log(name[1])
+console.log(name)
+let greeting = 'Good Morning'
+console.log(greeting.length)
+
+// Template Literals
+let boy1 = "Rohan"
+let boy2 = "Ronak"
+let sentance = `${boy1} is a friend of ${boy2}`
+console.log(sentance)
+
+// Escape sequence characters
+let fruit = 'Bana\'na'      // Single quote
+console.log(fruit)
+let fruit2 = "Oran\nge"    // New line
+console.log(fruit2)
+let fruit3 = "App\tle"    // Tab
+console.log(fruit3)
+
+let fruit4 = "Man\rgo"     // Carriage return
+console.log(fruit4)
+let x = "Hello\rGem"   // Gemlo
+console.log(x)
+
+let fruit5 = "Grap\be"    // Backspace
+console.log(fruit5)
+let fruit6 = "Pine\\apple" // Backslash
+console.log(fruit6)
