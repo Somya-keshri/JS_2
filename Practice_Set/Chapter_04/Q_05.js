a638c70c404da44f3397a8c08d032e5a89e9c1c7
Somya Keshri <keshrisomya9596@gmail.com>
2025-09-30 00:26:37 +0530

    ques

---
+++ Practice_Set/Chapter_04/Q_05.js
@@ -0,0 +1,5 @@
+// Try to change 4th character of a given string were you able to do it
+
+let friend = "Ritika"
+friend[2] = 's';
+console.log(friend)  // it will not change because string are immutable in js
