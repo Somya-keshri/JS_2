6f1f112319cdb5cf996f42799e589866b2a1fc2c
Somya Keshri <keshrisomya9596@gmail.com>
2025-09-30 00:25:58 +0530

    ques

---
+++ Practice_Set/Chapter_04/Q_02.js
@@ -0,0 +1,6 @@
+// Explore the includes, startswith and endswith functions of a string
+
+let str = "HelloWorld";
+console.log(str.includes("o"));
+console.log(str.startsWith("H"))
+console.log(str.endsWith("m")) 
