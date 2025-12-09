82e1b24945abc5f573745828781ddd081ae55158
Somya Keshri <keshrisomya9596@gmail.com>
2025-12-10 00:25:39 +0530

    Changing HTML Classes

---
+++ 05_HTML_Manipulation&Events/45_script.js
@@ -0,0 +1,8 @@
+first.className = "text-black yellow";
+console.log(first.classList);
+
+//first.classList.remove("yellow");
+//first.classList.add("red");
+//first.classList.toggle("yellow");
+
+console.log(first.classList.contains("red"));
