6f1aa673d0f145a370610f553c7710ac0f2f2208
Somya Keshri <keshrisomya9596@gmail.com>
2025-12-09 23:45:31 +0530

    insertAdjacentHTML, etc.

---
+++ 05_HTML_Manipulation&Events/44_script.js
@@ -0,0 +1,6 @@
+first.insertAdjacentHTML('beforebegin', 'div class="test"> Before Begin </div>');
+first.insertAdjacentHTML('beforeend', 'div class="test"> Before End </div>');
+first.insertAdjacentHTML('afterbegin', 'div class="test"> After Begin </div>');
+first.insertAdjacentHTML('afterend', 'div class="test"> After End </div>');
+
+second.remove();
