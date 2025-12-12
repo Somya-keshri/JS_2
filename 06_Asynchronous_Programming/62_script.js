fdbb5c5575c7f741e99d58255cca28e252c52b99
Somya Keshri <keshrisomya9596@gmail.com>
2025-12-13 02:26:34 +0530

    finally Clause

---
+++ 06_Asynchronous_Programming/62_script.js
@@ -0,0 +1,11 @@
+try {
+    let a = 0
+    console.log(program);
+    console.log("Program ran successfully ");
+} catch (err) {
+    console.log(err)
+}
+finally{
+    console.log("This will run anyway")
+}
+console.log("End")
